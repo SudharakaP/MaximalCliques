969fde10ebfe46c8e233590b10d966ea0db91611
Sudharaka Palamakumbura <sudharakap@mun.ca>
2016-04-08 13:03:45 -0230

    Remove unnecessary files and folders from project
    Some of the files in this projects were related to the previous project
    that I did starting from the same class structure. This commit removes
    them.

--- src/util/GraphLoader.java
+++ src/util/GraphLoader.java
@@ -1,51 +1,53 @@
-/**
- * @author UCSD MOOC development team
- * 
- * Utility class to add vertices and edges to a graph
- *
- */
 package util;
 
 import java.io.File;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
-
 import graph.Vertex;
 
+/**
+ * @author Sudharaka Palamakumbura
+ *
+ */
 public class GraphLoader {
-    /**
-     * Loads graph with data from a file.
-     * The file should consist of lines with 2 integers each, corresponding
-     * to a "from" vertex and a "to" vertex.
-     */ 
-    public static void loadGraph(graph.Graph g, String filename) {
-        Set<Integer> seen = new HashSet<Integer>();
-        Scanner sc;
-        try {
-            sc = new Scanner(new File(filename));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        // Iterate over the lines in the file, adding new
-        // vertices as they are found and connecting them with edges.
-        while (sc.hasNextInt()) {
-            int v1 = sc.nextInt();
-            int v2 = sc.nextInt();
-            if (!seen.contains(v1)) {
-            	Vertex v = new Vertex(v1);
-                g.addVertex(v);
-                seen.add(v1);
-            }
-            if (!seen.contains(v2)) {
-            	Vertex v = new Vertex(v2);
-                g.addVertex(v);
-                seen.add(v2);
-            }
-            g.addEdge(new Vertex(v1), new Vertex(v2));
-        }
-        
-        sc.close();
-    }
+
+	/**
+	 * Loads data from a file to graph. The file should contain data as an adjacency matrix
+	 * with each element separated by ,(commas) and each row separated by square brackets.
+	 * eg: [1,2,3],[5,7,6],[9,4,2] represents an adjacency matrix with three rows and columns. 
+	 * 
+	 * @param g
+	 * @param filename
+	 */
+	public static void graphLoader(graph.Graph g, String filename){
+		
+		Scanner sc;
+		try{
+			sc = new Scanner(new File(filename));
+		}catch (Exception e){
+			e.printStackTrace();
+			return;
+		}
+		
+		int row_index = 0;
+		sc.useDelimiter("\\[|\\],\\[|\\]");
+		while (sc.hasNext()){
+			row_index ++;
+			String row = sc.next();
+			
+			int column_index = 0;
+			Vertex v_row = new Vertex(row_index);
+			g.addVertex(v_row);
+			
+			for (String e: row.split(",")){
+				column_index ++;				
+				
+				if (Integer.parseInt(e) == 1){
+					Vertex v_col = new Vertex(column_index);
+					g.exportGraph().get(v_row).add(v_col);
+					g.addEdge(v_row, v_col);
+				}
+			}			
+		}
+		sc.close();
+	}
 }
