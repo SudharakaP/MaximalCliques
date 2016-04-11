21cd3e2df63d781c85fbd7f69a9dda32496dffa1
Sudharaka Palamakumbura <sudharakap@mun.ca>
2016-04-11 16:22:29 -0230

    Create class stub to implement Bron-Kerbosch algorithm
    - The Bron Kerbosch algorithm is used to find all maximal cliques of a
    graph. Further details are given here:
    https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm

---
+++ src/graph/BronKerbosch.java
@@ -0,0 +1,13 @@
+package graph;
+
+/**
+ * This class implements the Bron-Kerbosh algorithm for finding all Maximal Cliques
+ * in a graph. (https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm)
+ * 
+ * @author Sudharaka Palamakumbura
+ *
+ */
+public class BronKerbosch {
+	
+	
+}
