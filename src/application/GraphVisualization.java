53879989b359c6c2076db92e3765e41ac4c78899
Sudharaka Palamakumbura <sudharakap@mun.ca>
2016-04-18 20:01:56 -0230

    Add comments to GraphVisualization class

--- src/application/GraphVisualization.java
+++ src/application/GraphVisualization.java
@@ -1,10 +1,10 @@
 package application;
 
+import java.awt.event.MouseAdapter;
 import java.util.Set;
 
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
-import org.graphstream.ui.view.Viewer;
 import graph.CapGraph;
 import graph.Vertex;
 import util.GraphLoader;
@@ -16,9 +16,7 @@ import util.GraphLoader;
  * @author Sudharaka Palamakumbura
  *
  */
-public class GraphVisualization {
-	
-	Viewer graphView;
+public class GraphVisualization extends MouseAdapter{
 	
 	/**
 	 * Loads the graph.
@@ -27,14 +25,21 @@ public class GraphVisualization {
 	 */
 	public void GraphLoad(){
 		
+		// Load LinkedIn graph
 		CapGraph graph = new CapGraph();
 		GraphLoader.graphLoader(graph, "data/LinkedInReduced.txt");
 		
+		// Create a new Graph object in the GraphStream library
 		Graph graphDisplay = new SingleGraph("Social Network Graph");
 		
-		for (Vertex v: graph.exportGraph().keySet())
-			graphDisplay.addNode("" + v.getValue());
+		// Add each vertex to the GraphStream graph along with the vertex number
+		for (Vertex v: graph.exportGraph().keySet()){
+			Node n = graphDisplay.addNode("" + v.getValue());
+			n.addAttribute("ui.label", "" + v.getValue());
 			
+		} 
+		
+		// Add all edges to the GraphStream graph
 		for (graph.Edge e: graph.getEdges()){
 			int node1 = e.getVertices().get(0).getValue();
 			int node2 = e.getVertices().get(1).getValue();
@@ -43,17 +48,20 @@ public class GraphVisualization {
 			}catch (EdgeRejectedException | IdAlreadyInUseException exception){}
 		}
 		
+		// Change edge color of graph to "darkslategray"
+		graphDisplay.addAttribute("ui.stylesheet", "edge { fill-color: darkslategray; }");
+		
+		// Set maximum clique
 		Set<Vertex> maxClique = graph.maximumCliques(1);
 		
+		// Display vertices corresponding to the maximum clique in red color
 		for (Vertex v: maxClique)
 			graphDisplay.getNode("" + v.getValue()).addAttribute("ui.style", "fill-color: red;");
-
 		
-		graphDisplay.addAttribute("ui.stylesheet", "edge { fill-color: darkslategray; }");
-		graphView = graphDisplay.display();
-		//System.out.println(maxClique);
+		// Display the graph
+		graphDisplay.display();	
 	}
-	
+		
 	public static void main(String[] args){
 		GraphVisualization graphVisu = new GraphVisualization();
 		graphVisu.GraphLoad();
